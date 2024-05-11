e98a93e9fc73897b5dbed51748a3a92295de7815
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-05-11 22:56:25 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,41 @@
+window.onload=()=>{
+  
+  let cgt = document.querySelector(".cgt")
+  let wht = document.querySelector(".wht")  
+  let edt = document.querySelector(".edt")
+  let vat = document.querySelector(".vat")
+  
+  //Tax Divs
+  
+  let cgtDiv = document.querySelector(".cgt__div")
+   
+  
+  let taxBtn = document.querySelectorAll(".tax__btn")
+  
+  taxBtn.forEach((btn)=>{
+     btn.addEventListener("click",()=>{
+      if(btn.classList.contains("cgt")){
+          cgtDiv.classList.toggle("active")
+      }
+  }) 
+  })
+  
+   
+    
+    
+    
+    
+    
+    
+    
+}
 
+/*
+
+All display none 
+only display on click
+toggle display 
+tax functions
+
+*/
+       
